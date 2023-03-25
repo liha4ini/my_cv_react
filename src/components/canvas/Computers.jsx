9a7b213b48ffc4f6c0b736aef63b270d6103a316
liha4ini <liha4ev.sergey@gmail.com>
2023-03-25 16:10:54 +0300

    add Hero section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -13,6 +13,14 @@ const Computers = () => {
             <hemisphereLight intensity={0.15}
             groundColor='black' />
             <pointLight intensity={1} />
+            <spotLight
+                position={[-20, 50, 10]}
+                angle={0.12}
+                penumbra={1}
+                intensity={1}
+                castShadow
+                shadow-mapSize={1024}
+            />
             <primitive
                 object={computer.scene}
                 scale={0.75}
@@ -45,4 +53,4 @@ const ComputerCanvas = () => {
     )
 }
 
-export default Computers
+export default ComputerCanvas
